1ce6eb495103ecfa7c8efdb8b80209dfccc08966
Eduardo Bouças <mail@eduardoboucas.com>
2018-07-12 15:06:37 +0100

    fix: give priority to root route

--- src/util.js
+++ src/util.js
@@ -62,7 +62,7 @@ export function pathRankSort(a, b) {
 	if (aAttr.default) return 1;
 	if (bAttr.default) return -1;
 	let diff = rank(aAttr.path) - rank(bAttr.path);
-	return diff || -1;
+	return diff || (bAttr.path === '/' ? 1 : -1);
 }
 
 export function segmentize(url) {
